79e544a028fbc78714609ffcb1d6ceb9455cc62b
Hoàng Văn Việt Anh <hoangvanvietanh@gmail.com>
2020-08-01 16:09:29 +0700

    done

--- Xu_Ly_Dich_Vu/XU_LY_DU_LIEU/firebase.js
+++ Xu_Ly_Dich_Vu/XU_LY_DU_LIEU/firebase.js
@@ -40,6 +40,66 @@ class xu_ly_nhan_vien {
         }
     }
 
+
+    //////////
+    async Ghi_moi_Doi_tuong(Loai_Doi_tuong, Doi_tuong,id) {
+
+        try {
+            var Kq = await admin.firestore().collection(Loai_Doi_tuong).doc(id).set(Doi_tuong)
+            //var Kq = await db.collection(Loai_Doi_tuong).insert(Doi_tuong)
+            return Kq
+
+        } catch (Loi) {
+            console.log(Loi)
+        }
+    }
+
+    async Ghi_moi_Doi_tuong_Khong_Id(Loai_Doi_tuong, Doi_tuong) {
+
+        try {
+            var Kq = await admin.firestore().collection(Loai_Doi_tuong).doc().set(Doi_tuong)
+            //var Kq = await db.collection(Loai_Doi_tuong).insert(Doi_tuong)
+            return Kq
+
+        } catch (Loi) {
+            console.log(Loi)
+        }
+    }
+
+    async Them_lich_bieu(Loai_Doi_tuong, Doi_tuong,id) {
+
+        try {
+            var Kq = await admin.firestore().collection(Loai_Doi_tuong).doc(id).set(Doi_tuong)
+            //var Kq = await db.collection(Loai_Doi_tuong).insert(Doi_tuong)
+            return Kq
+
+        } catch (Loi) {
+            console.log(Loi)
+        }
+    }
+
+    async Cap_nhat_Doi_tuong(Loai_Doi_tuong, Doi_tuong,id) {
+
+        try {
+            var Kq = await admin.firestore().collection(Loai_Doi_tuong).doc(id).set(Doi_tuong)
+            //var Kq = await db.collection(Loai_Doi_tuong).insert(Doi_tuong)
+            return Kq
+
+        } catch (Loi) {
+            console.log(Loi)
+        }
+    }
+
+    async Xoa_Doi_tuong(Loai_Doi_tuong, Bieu_thuc_dieu_kien) {
+        try {
+            var db = await DbConnection.Get()
+            var Kq = await db.collection(Loai_Doi_tuong).remove(Bieu_thuc_dieu_kien);
+            return Kq
+        } catch (Loi) {
+            console.log(Loi);
+        }
+    }
+
 }
 
 var xuly = new xu_ly_nhan_vien;
